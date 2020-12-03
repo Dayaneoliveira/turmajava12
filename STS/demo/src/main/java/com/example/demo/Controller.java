0c0d39b00c54354efaf2decfc727650438041a4f
Dayaneoliveira <dayane873@gmail.com>
2020-12-03 11:11:53 -0300

    STS CRUD BACK-END BLOG PESSOAL

--- STS/demo/src/main/java/com/example/demo/Controller.java
+++ STS/demo/src/main/java/com/example/demo/Controller.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.util.Optional;
-import java.util.List;
 
 //é uma api rest que será consumida pelo front
 //WebMvcConfigurer é que faz o roteamento
@@ -29,29 +33,68 @@ public class Controller implements WebMvcConfigurer {
 	// @autowired injecao e repository é o caminhao que leva os dados na tabela
 	@Autowired
 	private ManutencaoRepository repository;
+	private Object resp;
+
+	// post inserir
+	@PostMapping("/post1")
+	public ManutencaoTable criar(@RequestBody ManutencaoTable objetinho) // metodo e o requestBody
+	{
+		repository.save(objetinho);// salvar na tabela
+		return objetinho; // retornar objeto
+	}
 
 	// este metodo criar vai salvar as informacoes la no banco
 	// insert ---------localhost:8080/pagina/manutencoes
-	@PostMapping("/manutencoes")
-	public ManutencaoTable criar(@RequestBody ManutencaoTable objetinho) {
-		repository.save(objetinho);
-		return objetinho;
+	@PostMapping("/post2") // metodo com Response
+	public ResponseEntity<ManutencaoTable> post(@RequestBody ManutencaoTable objeto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(objeto));// retorna cria e salva
 	}
-
 	// Find All
 	// Save
 	// FindByid
 	// deleteByid
 	// insert into tb_manutencao "OQUE A PESSOA DIGITAR NO BODY"
-	@GetMapping("/manutencoes/{id}")
-	public Optional<ManutencaoTable> encontrarUm(@PathVariable Long id) {
-		return repository.findById(id);
-	}
 
-	@GetMapping("/manutencoes")
+	// getAll select *
+	// from------------------------------------------------------------------------------------------
+	@GetMapping("/getAll1")
 	public List<ManutencaoTable> pesquisarTodos() {
 		return repository.findAll();
 	}
+
+	@GetMapping("/getAll2")
+	public ResponseEntity<List<ManutencaoTable>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	// LOCALHOST:8080/pagina/getById1/1
+	// gets by id select where
+	// id-----------------------------------------------------------------------------------
+	@GetMapping("/getById1/{id}")
+	public Optional<ManutencaoTable> encontrarUm(@PathVariable Long id) {
+		return repository.findById(id);
+
+	}
+
+	// LOCALHOST:8080/pagina/getById2/1
+	@GetMapping("/getById2/{id}")
+	public ResponseEntity<ManutencaoTable> GetById(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	// updates
+	@PutMapping("/put1/{id}")
+	public ManutencaoTable atualizar(@PathVariable Long id, @RequestBody ManutencaoTable objetinho) {
+		objetinho.setId(id);
+		repository.save(objetinho);
+		return objetinho;
+	}
+
+	@PutMapping("/put2")
+	public ResponseEntity<ManutencaoTable> put(@RequestBody ManutencaoTable postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+
 }
 // SELECT * FROM TB_MANUTENCAO WHERE id={id}
 //path variable serve para passar parametro 
